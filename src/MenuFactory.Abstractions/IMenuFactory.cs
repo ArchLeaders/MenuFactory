55f327e82d5d017ff36e562b066736c64b800848
ArchLeaders <archleadership28@gmail.com>
2024-08-28 13:42:23 -0700

    Create abstractions library

---
+++ src/MenuFactory.Abstractions/IMenuFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+namespace MenuFactory.Abstractions;
+
+public interface IMenuFactory
+{
+    IEnumerable Items { get; }
+
+    /// <summary>
+    /// Add a new menu group from a menu model.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="groupId">The ID of the group to add.</param>
+    /// <param name="source">An instance of <typeparamref name="T"/> for non-static methods.</param>
+    void AddMenuGroup<T>(string? groupId = null, object? source = null);
+
+    /// <inheritdoc cref="RemoveMenuGroup(string)"/>
+    bool RemoveMenuGroup<T>(string? groupId = null) => RemoveMenuGroup(groupId ?? typeof(T).Name);
+
+    /// <summary>
+    /// Remove an existing menu group.
+    /// </summary>
+    /// <param name="groupId">The ID of the group to remove.</param>
+    /// <returns><see langword="true"/> is the group was found, <see langword="false"/> is the <paramref name="groupId"/> could not be found.</returns>
+    bool RemoveMenuGroup(string groupId);
+}
